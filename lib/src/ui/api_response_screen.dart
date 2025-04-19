638a988a5610e4d8c770b9053c15552d748462f4
hbutt <48251967+HassanButt2019@users.noreply.github.com>
2025-04-19 21:22:59 +0200

    Update api_response_screen.dart

--- lib/src/ui/api_response_screen.dart
+++ lib/src/ui/api_response_screen.dart
@@ -2,7 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../utils/json_pretifier.dart';
-import '../models/api_test_data.dart';// lib/src/ui/api_response_screen.dart
+import '../models/api_test_data.dart'; // lib/src/ui/api_response_screen.dart
+
 class ApiResponseScreen extends StatefulWidget {
   final ApiTestData api;
 
@@ -18,12 +19,13 @@ class _ApiResponseScreenState extends State<ApiResponseScreen> {
   @override
   Widget build(BuildContext context) {
     final rawResponse = widget.api.responseBody ?? '';
+    final rawRequest = widget.api.requestBody ?? '';
 
     // This value will change every time `prettifyJson` changes
-    final responseText = prettifyJson
-        ? JsonPrettifier.pretty(rawResponse)
-        : rawResponse;
-
+    final responseText =
+        prettifyJson ? JsonPrettifier.pretty(rawResponse) : rawResponse;
+    final requestTest =
+        prettifyJson ? JsonPrettifier.pretty(rawRequest) : rawRequest;
     return Scaffold(
       appBar: AppBar(
         title: const Text('API Response'),
@@ -59,15 +61,22 @@ class _ApiResponseScreenState extends State<ApiResponseScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('URL: ${widget.api.url}', style: const TextStyle(fontWeight: FontWeight.bold)),
+            Text('URL: ${widget.api.url}',
+                style: const TextStyle(fontWeight: FontWeight.bold)),
             const SizedBox(height: 8),
             Text('Method: ${widget.api.method}'),
             const SizedBox(height: 8),
             Text('Status: ${widget.api.statusCode}'),
             const SizedBox(height: 8),
             Text('Timestamp: ${widget.api.timestamp}'),
+            const SizedBox(height: 8),
+            const Text('Request Body:',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            const SizedBox(height: 8),
+            SelectableText(requestTest),
             const SizedBox(height: 16),
-            const Text('Response Body:', style: TextStyle(fontWeight: FontWeight.bold)),
+            const Text('Response Body:',
+                style: TextStyle(fontWeight: FontWeight.bold)),
             const SizedBox(height: 8),
             SelectableText(responseText),
           ],
